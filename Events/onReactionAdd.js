552dbb9d2a029601bb9e9735f948dfba4782c92b
DONFN <kiagias@protonmail.com>
2021-09-16 23:16:20 +0300

    ++ reactions

--- Events/onReactionAdd.js
+++ Events/onReactionAdd.js
@@ -17,44 +17,47 @@ module.exports = {
             let {guild} = reaction.message;
             let member = guild.members.cache.get(user.id);
             let emoji = reaction._emoji.name
-            switch (emoji) {
-                case '🖥️':
-                    member.roles.add("886993717725102103")
-                    break;
-                case '🤖':
-                    member.roles.add("887198468421083136")
-                    break;
-                case '⚙️':
-                    member.roles.add("887198536360419348")
-                    break;
-                case '⚖️':
-                    member.roles.add("887198604027101284")
-                    break;
-                case '🌱':
-                    member.roles.add("887198628899356683")
-                    break;
-                case '💉':
-                    member.roles.add("887198656837591041")
-                    break;
-                case '🎶':
-                    member.roles.add("887200015825641542")
-                    break;
-                case '💼':
-                    member.roles.add("887198689372815410")
-                    break;
-                case '✈️':
-                    member.roles.add("887198713628483664")
-                    break;
-                case '🍎':
-                    member.roles.add("887198734209925141")
-                    break;
-                case '💸':
-                    member.roles.add("887198782255661086")
-                    break;
-                default:
-                    console.log(emoji);
-                    console.log("No roles are attached to this emoji");
-            }
+
+            if(reaction.message.channel.id == "886981920108478526"){
+                        switch (emoji) {
+                            case '🖥️':
+                                member.roles.add("886993717725102103")
+                                break;
+                            case '🤖':
+                                member.roles.add("887198468421083136")
+                                break;
+                            case '⚙️':
+                                member.roles.add("887198536360419348")
+                                break;
+                            case '⚖️':
+                                member.roles.add("887198604027101284")
+                                break;
+                            case '🌱':
+                                member.roles.add("887198628899356683")
+                                break;
+                            case '💉':
+                                member.roles.add("887198656837591041")
+                                break;
+                            case '🎶':
+                                member.roles.add("887200015825641542")
+                                break;
+                            case '💼':
+                                member.roles.add("887198689372815410")
+                                break;
+                            case '✈️':
+                                member.roles.add("887198713628483664")
+                                break;
+                            case '🍎':
+                                member.roles.add("887198734209925141")
+                                break;
+                            case '💸':
+                                member.roles.add("887198782255661086")
+                                break;
+                            default:
+                                break;
+                            }
+                            reaction.message.reactions.cache.get(emoji).users.remove(user);
+        }
         })
     }
 }
