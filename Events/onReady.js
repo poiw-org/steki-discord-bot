cc84797e6acc92ff6dd9633c95946c65cdfbf2c8
DONFN <kiagias@protonmail.com>
2021-09-24 16:46:07 +0300

    Added a spammer for the registrations channel

--- Events/onReady.js
+++ Events/onReady.js
@@ -23,11 +23,22 @@ module.exports = {
                 embedSetupBeta.setup(bot)
             })
             //SetupSupport message
-            setInterval(() => {
-                const index = Math.floor(Math.random() * (activities.length - 1) + 1)
-                bot.user.setActivity(activities[index])
-            }, 10000)
+            bot.channels.fetch("886981920108478526").then(channel=>{
+                setInterval(() => {
+                    const index = Math.floor(Math.random() * (activities.length - 1) + 1)
+                    bot.user.setActivity(activities[index])
+
+                    channel.send("Το Steki για εσένα τώρα φαίνεται κενό και αυτό είναι γιατί δεν έχεις γραφτεί.\n" +
+                        "\n" +
+                        "    Για να ολοκληρώσεις την εγγραφή σου στο Steki, βρες σε αυτό το κανάλι/δωμάτιο το μήνυμα που αναφέρει το όνομά σου στο Discord και πάτα στο κανάλι εγγραφής που σου δείχνει (#register-[τυχαίος αριθμός])!\n" +
+                        "\n" +
+                        "    Αν δεν βρίσκεις το μήνυμα μπορείς να πας στα κανάλια του Steki και να βρεις το κανάλι εγγραφής σου (αν είσαι από κινητό, σύρε από την αριστερή άκρη προς τη μέση του κινητού ή πάτα τις 3 οριζόντιες γραμμές πάνω αριστερά).\n" +
+                        "\n" +
+                        "Αν αντιμετωπίζεις κάποιο πρόβλημα ή δεν ξέρεις τι να κάνεις, στείλε σε ένα από αυτά τα άτομα  -> <@458700490348429322> , <@690205775448244301> , <@809804139513118781>")
+                }, 60000)
+            })
 
         })
     }
 }
+
